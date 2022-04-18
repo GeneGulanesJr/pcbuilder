ef3ccffede14f6236785d37cd2e54f23104b52ca
GeneGulanesJr <ggulanes_180000000405@uic.edu.ph>
2022-04-18 13:05:30 +0800

    Updates

---
+++ precache-manifest.c1f4940ae43814ad10f8341772e59d27.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659895722e79bd3cf48a1480841d2de5",
+    "url": "/pcbuilder/index.html"
+  },
+  {
+    "revision": "27819ed2d009a7d14345",
+    "url": "/pcbuilder/static/css/2.bff19397.chunk.css"
+  },
+  {
+    "revision": "509ee1aa0b69d7bd1a5a",
+    "url": "/pcbuilder/static/css/main.97aa01cc.chunk.css"
+  },
+  {
+    "revision": "27819ed2d009a7d14345",
+    "url": "/pcbuilder/static/js/2.a3e99362.chunk.js"
+  },
+  {
+    "revision": "509ee1aa0b69d7bd1a5a",
+    "url": "/pcbuilder/static/js/main.2c96f222.chunk.js"
+  },
+  {
+    "revision": "2523288887e2734b592f",
+    "url": "/pcbuilder/static/js/runtime~main.93241804.js"
+  },
+  {
+    "revision": "254a49b526cbc62a37431d657e8bb456",
+    "url": "/pcbuilder/static/media/2070.254a49b5.png"
+  },
+  {
+    "revision": "422478b9b4b813118f2f7548140bab49",
+    "url": "/pcbuilder/static/media/2080.422478b9.png"
+  },
+  {
+    "revision": "1c96c870b3ddc593bb41bd279c6a9530",
+    "url": "/pcbuilder/static/media/acer.1c96c870.png"
+  },
+  {
+    "revision": "724b19e41dbafd6fcb1977346dbcc56b",
+    "url": "/pcbuilder/static/media/acer.724b19e4.gif"
+  },
+  {
+    "revision": "353fe068c14ba04fc33c677342e0a3c0",
+    "url": "/pcbuilder/static/media/amd.353fe068.png"
+  },
+  {
+    "revision": "56856f838f30efb8f0277bb72167ebba",
+    "url": "/pcbuilder/static/media/asus.56856f83.gif"
+  },
+  {
+    "revision": "5d44601309617dd8b13493db653cb9cb",
+    "url": "/pcbuilder/static/media/asus.5d446013.png"
+  },
+  {
+    "revision": "660ddc43ce01fd50fb7b58587b03b8ae",
+    "url": "/pcbuilder/static/media/asus.660ddc43.png"
+  },
+  {
+    "revision": "28bae0a45a0e9c4c59948a6c4745d79a",
+    "url": "/pcbuilder/static/media/benq.28bae0a4.gif"
+  },
+  {
+    "revision": "2c8886205305ce1e8ebc030da8ba6992",
+    "url": "/pcbuilder/static/media/benq.2c888620.png"
+  },
+  {
+    "revision": "d977be7f3260034b2a7015338f19a3d9",
+    "url": "/pcbuilder/static/media/bg.d977be7f.jpg"
+  },
+  {
+    "revision": "d7e7eeea0cf447cfd3a2fb74acf40838",
+    "url": "/pcbuilder/static/media/bg2.d7e7eeea.jpg"
+  },
+  {
+    "revision": "9ae2dabbca7e802ceae834bcbe2bbcaf",
+    "url": "/pcbuilder/static/media/bit.9ae2dabb.png"
+  },
+  {
+    "revision": "b4c2ec6d95f6e5e2b4966c5efc662d04",
+    "url": "/pcbuilder/static/media/cabinet1.b4c2ec6d.png"
+  },
+  {
+    "revision": "4ce85dfa9d39fa8b228fd19b161144de",
+    "url": "/pcbuilder/static/media/cor.4ce85dfa.png"
+  },
+  {
+    "revision": "6442c6f920c1a0af364f7d8f0370ff6b",
+    "url": "/pcbuilder/static/media/cor.6442c6f9.png"
+  },
+  {
+    "revision": "e5dbbbbaba03c08f3c82a24e4eed16be",
+    "url": "/pcbuilder/static/media/evga.e5dbbbba.png"
+  },
+  {
+    "revision": "b7c86ac49b567e3229126ed678d4ca78",
+    "url": "/pcbuilder/static/media/gigam.b7c86ac4.png"
+  },
+  {
+    "revision": "414401dc43a0aeafb74388636a25e4f8",
+    "url": "/pcbuilder/static/media/glass.414401dc.png"
+  },
+  {
+    "revision": "a2e1010b96fd93297ca0be40deb843ca",
+    "url": "/pcbuilder/static/media/gsk.a2e1010b.png"
+  },
+  {
+    "revision": "95dab9527568366807e06702c91e6e7a",
+    "url": "/pcbuilder/static/media/hyper.95dab952.png"
+  },
+  {
+    "revision": "f1f43b7f2eedfbb37722e3f9b2e9b9a1",
+    "url": "/pcbuilder/static/media/i7.f1f43b7f.png"
+  },
+  {
+    "revision": "45dcc59a8ab4eb1b56fc387e8ec25730",
+    "url": "/pcbuilder/static/media/i9o.45dcc59a.png"
+  },
+  {
+    "revision": "2ce0871dde78eac57f75c3fe1a34466d",
+    "url": "/pcbuilder/static/media/msib.2ce0871d.png"
+  },
+  {
+    "revision": "b7ed609365cc46b31f88424b72851eb2",
+    "url": "/pcbuilder/static/media/msigc.b7ed6093.png"
+  },
+  {
+    "revision": "49de8205eaca38de9e8e8b2e25e777e3",
+    "url": "/pcbuilder/static/media/mus.49de8205.png"
+  },
+  {
+    "revision": "4328e6bf22d36cda76c2f1c1c692a0a7",
+    "url": "/pcbuilder/static/media/noc.4328e6bf.png"
+  },
+  {
+    "revision": "63115e6a822132b48e169a3cb5d6cedb",
+    "url": "/pcbuilder/static/media/nzxt.63115e6a.png"
+  },
+  {
+    "revision": "a19fd3fd9cecad3e966d100b3f8b8bfc",
+    "url": "/pcbuilder/static/media/sam.a19fd3fd.png"
+  },
+  {
+    "revision": "4d1952aefd867728e626285198cf554a",
+    "url": "/pcbuilder/static/media/sea.4d1952ae.jpg"
+  },
+  {
+    "revision": "982e49f3c8268ae2c82d8f8077bc1d47",
+    "url": "/pcbuilder/static/media/sea2.982e49f3.jpg"
+  },
+  {
+    "revision": "c0e8fe5b25477b8c71d11940c63c173a",
+    "url": "/pcbuilder/static/media/seagate.c0e8fe5b.png"
+  },
+  {
+    "revision": "a17be52fa4cbf3e3e56415f6b63279a5",
+    "url": "/pcbuilder/static/media/tosh.a17be52f.jpg"
+  },
+  {
+    "revision": "3c9af3c1af438289a0160cdc7a8f682d",
+    "url": "/pcbuilder/static/media/wd.3c9af3c1.png"
+  },
+  {
+    "revision": "e0eeee7a6de5cf3253b4b195bb6b035f",
+    "url": "/pcbuilder/static/media/zorque.e0eeee7a.ttf"
+  }
+]);
